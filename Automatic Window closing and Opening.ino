c8fb89fb0e5740e8febee30efcaf283440ef0604
NagullugaN <52689710+Nagul232@users.noreply.github.com>
2020-01-28 21:40:49 +0530

    Added the  command lines

--- Automatic Window closing and Opening.ino
+++ Automatic Window closing and Opening.ino
@@ -1,6 +1,6 @@
     // include the library code:
-    #include <LiquidCrystal.h>
-    #include<Servo.h>
+    #include <LiquidCrystal.h> //header file for LCD display
+    #include<Servo.h>  //Header for Servo motor
     int rainvalue;
     int rainsensor;
     int ldr=A0;
@@ -11,26 +11,26 @@
      float temp;
 
     const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-    LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+    LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  // LCD display pin  configuration
 
     void setup() {
-     servo1.attach(9);
-      lcd.begin(16, 2);
-      Serial.begin(9600);
+     servo1.attach(9);  //PWM pin for Servo 
+      lcd.begin(16, 2);  //the LCD display configuration
+      Serial.begin(9600);  // serial begin to use the serial monitor
 
     }
 
     void loop()
     {
-      rainvalue= analogRead(rainsensor);
-      rainvalue=map(rainvalue,0,1023,0,255);
+      rainvalue= analogRead(rainsensor);  // to read the rain sensor value
+      rainvalue=map(rainvalue,0,1023,0,255); // mapping the rain value to 0 to 255
       Value=analogRead(A0);
       Value = map(Value,0,1023,0,255);
       Serial.print("ldr--");
       Serial.print(Value);
       Serial.println();
-     temp=analogRead(tempPin);
-      temp=(temp*500)/1023;
+     temp=analogRead(tempPin);  // temperature reading here i have used LM35 sensor
+      temp=(temp*500)/1023;  //to find the celcius
       lcd.setCursor(0,1);
       lcd.print("temperature is =");
       Serial.print("temperature is =");
@@ -52,7 +52,7 @@
       }
       for(positionCounter=0;positionCounter<50;positionCounter++)
       {
-        lcd.scrollDisplayLeft();
+        lcd.scrollDisplayLeft(); // to scroll the display
       delay(150);
       }
 
